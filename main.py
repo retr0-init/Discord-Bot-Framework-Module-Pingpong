88124b8cdf1103e5d2663df1befae59e9d2184cd
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-01-19 00:57:01 +0000

    update 3

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ class PingPong(interactions.Extension):
     @interactions.slash_command("ping", description="Ping pong!")
     async def ping(self, ctx: interactions.SlashContext):
         """A command to ping pong"""
-        await ctx.send("Pong2!")
+        await ctx.send("Pong3!")
